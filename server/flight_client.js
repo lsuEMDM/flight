1f17280c36becd46b473596c9124613d584db321
Ben Taylor <taylorbf@gmail.com>
2014-04-22 16:54:12 -0500

    Fixing the client!!
    Opening a socket forwarder from server to client

--- server/flight_client.js
+++ server/flight_client.js
@@ -1,14 +1,11 @@
-
-var connect = require('connect'),
-    http = require('http'),
-    osc = require('node-osc'),
+var osc = require('node-osc'),
     client = new osc.Client('localhost', 4040);   
-    console.log("http server on 8080, osc client on 4040");
+    console.log("socket.io client connected, osc client on 4040");
 
-var client = require('socket.io-client').connect("http://localhost:8080");
-client.on('connect', function () { 
+var ioclient = require('socket.io-client').connect("http://localhost:8080");
+ioclient.on('connect', function () { 
   console.log("socket connected"); 
-  client.on('fromwebsite', function (data) {
+  ioclient.on('fromserver', function (data) {
     client.send(data.oscname, data.val);
     console.log(data);
   });
--- server/flight_server.js
+++ server/flight_server.js
@@ -3,3 +3,11 @@ var connect = require('connect'),
     app = connect().use(connect.static(__dirname)).listen(8080),
     io = require('socket.io').listen(app);
     console.log("http server on 8080");
+
+
+io.sockets.on('connection', function (socket) {
+    socket.on('fromwebsite', function (data) {
+      socket.broadcast.emit("fromserver", data);
+      console.log(data);
+    });
+});
